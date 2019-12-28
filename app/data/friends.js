73925e1ca525fc00f719e5626065e9aae5bdb675
JeanCha <51973809+JeanCha@users.noreply.github.com>
2019-12-28 11:19:35 -0600

    main

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Marcy",
+		photo: "https://www.pexels.com/photo/man-wearing-a-jacket-sitting-on-brown-wooden-crate-594610/",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Archer",
+		photo: "https://www.pexels.com/photo/man-in-white-v-neck-t-shirt-and-black-pants-775358/",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Barney",
+		photo: "https://www.pexels.com/photo/photography-of-a-guy-wearing-green-shirt-1222271/",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Homer",
+		photo: "https://www.pexels.com/photo/silhouette-of-man-during-nighttime-1136575/",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Al",
+		photo: "https://www.pexels.com/photo/group-of-people-standing-on-beach-shore-1427741/",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
